64ea3bf4c1da93226b54f94dfcd5a3fd9314411f
Jake Sasser <sasserjacob@gmail.com>
2020-06-23 20:58:56 -0600

    show page

--- client/src/components/posts/PostList.js
+++ client/src/components/posts/PostList.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import Post from './Post';
+import { Link } from 'react-router-dom';
 
 const PostList = ({ posts, updatePost, deletePost }) => (
   <>
     { posts.map( p => 
-      <Post 
-        key={p.id}
-        {...p}
-        updatePost={updatePost}
-        deletePost={deletePost}
-      />
-    )
+      <Link to={{
+        pathname: `/posts/${p.id}`,
+        state: { ...p } 
+      }}>
+        <Post 
+          key={p.id}
+          {...p}
+          updatePost={updatePost}
+          deletePost={deletePost}
+        />
+      </Link>
+    ) 
     }
   </>
 )
-
 export default PostList;
